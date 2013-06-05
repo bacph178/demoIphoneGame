80a481ae8f070eca4e02c564fb62f98ef80ea3d6
macbook_016 <Admin@macbook-016.local>
2013-06-05 13:08:00 +0700

    Initial Commit

---
+++ demoIphoneGame/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  demoIphoneGameAppController.h
+//  demoIphoneGame
+//
+//  Created by macbook_016 on 2013/06/05.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ demoIphoneGame/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  demoIphoneGameAppController.h
+//  demoIphoneGame
+//
+//  Created by macbook_016 on 2013/06/05.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
